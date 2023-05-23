fd88bbe0dd84c431b0d280ea9b444870a3f2a727
Bianca Oliveira <babianca.sete@gmail.com>
2023-05-23 11:05:27 -0300

    first commit

---
+++ Farmacia/src/main/java/com/generation/Farmacia/model/Produto.java
@@ -0,0 +1,94 @@
+package com.generation.Farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.generation.Farmacia.model.Categoria;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+	@Entity
+	@Table(name= "tb_produto")
+	public class Produto {
+
+		
+		@Id //indica que esse atributo será uma chave primária na minha tabela
+		@GeneratedValue(strategy = GenerationType.IDENTITY) //indica que terá um auto-increment
+		private Long Id;
+		
+		@NotBlank(message = "Este atributo é de preenchimento obrigatório")
+		@Size(min = 1, max = 100, message = "Este atributo tem que ter no mínimo 1 caracteres e no máximo 100 caracteres")
+		private String nome;
+		
+		
+		@NotBlank(message = "Este atributo é de preenchimento obrigatório")
+		@Size(min = 1, max = 100, message = "Este atributo tem que ter no mínimo 1 caracteres e no máximo 1000 caracteres")
+		private String receita;
+		
+		@NotNull(message = "Este atributo é de preenchimento obrigatório")
+		private Integer quantidade;
+		
+		@NotNull(message = "Este atributo é de preenchimento obrigatório")
+		private Double preco;
+		
+		
+		
+		public Categoria getCategoria() {
+			return categoria;
+		}
+
+		public void setCategoria(Categoria categoria) {
+			this.categoria = categoria;
+		}
+
+		@ManyToOne
+		@JsonIgnoreProperties("produtos")
+	    private Categoria categoria;
+
+		public Long getId() {
+			return Id;
+		}
+
+		public void setId(Long id) {
+			Id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getReceita() {
+			return receita;
+		}
+
+		public void setReceita(String receita) {
+			this.receita = receita;
+		}
+
+		public Integer getQuantidade() {
+			return quantidade;
+		}
+
+		public void setQuantidade(Integer quantidade) {
+			this.quantidade = quantidade;
+		}
+
+		public Double getPreco() {
+			return preco;
+		}
+
+		public void setPreco(Double preco) {
+			this.preco = preco;
+		}
+}
